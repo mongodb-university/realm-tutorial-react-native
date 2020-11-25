5dec08d47ffcf4821fef35aa51642d210015e412
Nick Larew <nick.larew@10gen.com>
2020-11-25 11:13:46 -0500

    Remove final code from start

--- providers/AuthProvider.js
+++ providers/AuthProvider.js
@@ -27,30 +27,6 @@ const AuthProvider = ({ children }) => {
     const myProject = { name: "My Project", partition: `project=${user.id}` };
     setProjectData([myProject]);
 
-    const config = {
-      sync: {
-        user,
-        partitionValue: `user=${user.id}`,
-      },
-    };
-
-    // Open a realm with the logged in user's partition value in order
-    // to get the projects that the logged in user is a member of
-    Realm.open(config).then((userRealm) => {
-      realmRef.current = userRealm;
-      const users = userRealm.objects("User");
-
-      users.addListener(() => {
-        // The user custom data object may not have been loaded on
-        // the server side yet when a user is first registered.
-        if (users.length === 0) {
-          setProjectData([myProject]);
-        } else {
-          const { memberOf } = users[0];
-          setProjectData([...memberOf]);
-        }
-      });
-    });
     // TODO: Open the user realm, which contains at most one user custom data object
     // for the logged-in user.
 
