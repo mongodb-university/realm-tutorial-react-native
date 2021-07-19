e384ca84e40d9aafe8b68a3ee700d1a5099e2e43
chriswayg <chriswayg@gmail.com>
2021-07-19 22:45:15 +0800

    added actual AppId

--- getRealmApp.js
+++ getRealmApp.js
@@ -1,17 +1,20 @@
-import Realm from "realm";
+// To get the app working with your backend, you first need to instantiate the Realm app.
+// The Realm app is the interface to the MongoDB Realm backend.
+
+import Realm from 'realm';
 
 let app;
 
 // Returns the shared instance of the Realm app.
 export function getRealmApp() {
   if (app === undefined) {
-    const appId = "<your Realm app ID here>"; // Set Realm app ID here.
+    const appId = 'tasktracker-klsjn'; // Set Realm app ID here.
     const appConfig = {
       id: appId,
       timeout: 10000,
       app: {
-        name: "default",
-        version: "0",
+        name: 'default',
+        version: '0',
       },
     };
     app = new Realm.App(appConfig);
